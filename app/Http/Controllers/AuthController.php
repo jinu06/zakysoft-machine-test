29a2ada2f12936789e8fa61d7ba3c7234ea45858
Jinshaj <jinuwdr05@gmail.com>
2025-09-03 16:34:27 +0530

    user authentication flow added

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\saveUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class AuthController extends Controller
+{
+    public function register(saveUserRequest $request)
+    {
+        try {
+            $data = $request->validated();
+
+            $user =  User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password'])
+            ]);
+
+            $token = $user->createToken('Plain Token')->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Register successful',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        } catch (\Throwable $th) {
+            Log::error('user creation failed', ['error' => $th->getMessage()]);
+            return response()->json([
+                'status' => false,
+                'message' => 'server error'
+            ], 500);
+        }
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            $validated = $request->validated();
+
+            $user = User::where('email', $validated['email'])
+                ->first();
+
+            if (!$user || !Hash::check($validated['password'], $user->password)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Invalid credentials'
+                ], 422);
+            }
+
+            $token = $user->createToken('Plain Token')->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Login successful',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        } catch (\Throwable $th) {
+            Log::error('login failed', ['error' => $th->getMessage()]);
+            return response()->json([
+                'status' => false,
+                'message' => 'server error'
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'successfully logged out'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\InventoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/inventory/report', [InventoryController::class, 'report']);
 Route::post('/stock-movements', [InventoryController::class, 'storeStock'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
